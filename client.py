2e33466e9a00506cb9635776209d896cb6301635
pfnazhmi <pfnazhmi@gmail.com>
2023-12-19 17:32:43 +0700

    upodate GUI

--- client.py
+++ client.py
@@ -1,70 +1,86 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
 from xmlrpc.client import ServerProxy
-from datetime import datetime
 
-def main():
-    server = ServerProxy('http://localhost:8000')
+class AntreanMedisGUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Antrean Medis Telkomedika")
+        
+        self.server = ServerProxy('http://localhost:8000')
+        
+        self.create_widgets()
 
-    while True:
-        print("\nMenu:")
-        print("1. Daftar Poli")
-        print("2. Lihat Antrian di Poli")
-        print("3. Mendaftar di Poli")
-        print("4. Lihat Data Pasien")
-        print("0. Keluar")
+    def create_widgets(self):
+        self.label = tk.Label(self.master, text="Selamat Datang di Antrean Medis Telkomedika")
+        self.label.pack(pady=10)
 
-        choice = input("Pilih menu (0-4): ")
+        self.button_poli = tk.Button(self.master, text="Lihat Poli", command=self.daftar_poli)
+        self.button_poli.pack()
 
-        if choice == '0':
-            break
-        elif choice == '1':
+        self.button_lihat_antrian = tk.Button(self.master, text="Lihat Antrian", command=self.lihat_antrian)
+        self.button_lihat_antrian.pack()
+
+        self.button_mendaftar = tk.Button(self.master, text="Mendaftar", command=self.mendaftar)
+        self.button_mendaftar.pack()
+
+        self.button_lihat_data = tk.Button(self.master, text="Lihat Data Pasien", command=self.lihat_data)
+        self.button_lihat_data.pack()
+
+        self.button_exit = tk.Button(self.master, text="Keluar", command=self.master.destroy)
+        self.button_exit.pack(pady=10)
+
+    def daftar_poli(self):
+        try:
+            klinik_list = self.server.daftar_poli()
+            poli_str = "\n".join([f"{i}. {klinik}" for i, klinik in enumerate(klinik_list, 1)])
+            messagebox.showinfo("Daftar Poli", f"Daftar Poli di Telkomedika:\n{poli_str}")
+        except Exception as e:
+            messagebox.showerror("Kesalahan", f"Terjadi kesalahan: {e}")
+
+    def lihat_antrian(self):
+        klinik = simpledialog.askstring("Masukkan Nama Poli", "Masukkan nama poli:")
+        if klinik:
             try:
-                klinik_list = server.daftar_poli()
-                print("Daftar Poli di Telkomedika:")
-                for i, klinik in enumerate(klinik_list, 1):
-                    print(f"{i}. {klinik}")
-            except Exception as e:
-                print(f"Terjadi kesalahan: {e}")
-        elif choice == '2':
-            try:
-                klinik = input("Masukkan nama poli: ")
-                antrian = server.daftar_antrian(klinik)
-                waktu_tunggu = server.daftar_waktu_tunggu(klinik)
+                antrian = self.server.daftar_antrian(klinik)
+                waktu_tunggu = self.server.daftar_waktu_tunggu(klinik)
                 if not antrian:
-                    print(f'Tidak ada antrian di {klinik}')
+                    messagebox.showinfo("Antrian", f'Tidak ada antrian di {klinik}')
                 else:
-                    print(f'Antrian {klinik}:\n{antrian}\nWaktu Tunggu: {waktu_tunggu} menit')
+                    antrian_str = "\n".join([f"{data['nomor_antrean']}. {data['pasien']['nama']}" for data in antrian])
+                    messagebox.showinfo("Antrian", f'Antrian {klinik}:\n{antrian_str}\nWaktu Tunggu: {waktu_tunggu} menit')
             except Exception as e:
-                print(f"Terjadi kesalahan: {e}")
-        elif choice == '3':
-            try:
-                klinik_list = server.daftar_poli()
-                for i, klinik in enumerate(klinik_list, 1):
-                    print(f"{i}. {klinik}")
-                nama_poli = input("Masukkan nama klinik: ")
-                nomor_rekam_medis = input("Masukkan nomor rekam medis: ")
-                nama = input("Masukkan nama: ")
-                while True:
-                    try:
-                        tanggal_lahir = input("Masukkan tanggal lahir (YYYY-MM-DD): ")
-                        datetime.strptime(tanggal_lahir, '%Y-%m-%d')
-                        break 
-                    except ValueError:
-                        print("Format tanggal salah. Coba lagi.")
+                messagebox.showerror("Kesalahan", f"Terjadi kesalahan: {e}")
 
-                nomor_antrean, waktu_tunggu = server.tambah_antrian(nama_poli, {'nomor_rekam_medis': nomor_rekam_medis, 'nama': nama, 'tanggal_lahir': tanggal_lahir})
-                print(f'Anda telah mendaftar di {nama_poli}. Nomor Antrean: {nomor_antrean}\nPerkiraan waktu tunggu: {waktu_tunggu} menit')
-            except Exception as e:
-                print(f"Terjadi kesalahan: {e}")
-        elif choice == '4':
+    def mendaftar(self):
+        klinik = simpledialog.askstring("Masukkan Nama Poli", "Masukkan nama poli:")
+        nomor_rekam_medis = simpledialog.askstring("Nomor Rekam Medis", "Masukkan nomor rekam medis:")
+        nama = simpledialog.askstring("Nama", "Masukkan nama:")
+        tanggal_lahir = simpledialog.askstring("Tanggal Lahir", "Masukkan tanggal lahir (YYYY-MM-DD):")
+
+        if klinik and nomor_rekam_medis and nama and tanggal_lahir:
             try:
-                klinik = input("Masukkan Nama poli: ")
-                nomor_antrean = int(input("Masukkan Nama antrean: "))
-                pasien_data = server.lihat_data_pasien(klinik, nomor_antrean)
-                print(f'Data Pasien:\n{pasien_data}')
+                nomor_antrean, waktu_tunggu = self.server.tambah_antrian(
+                    klinik,
+                    {'nomor_rekam_medis': nomor_rekam_medis, 'nama': nama, 'tanggal_lahir': tanggal_lahir}
+                )
+                messagebox.showinfo("Mendaftar", f'Anda telah mendaftar di {klinik}. Nomor Antrean: {nomor_antrean}\nPerkiraan waktu tunggu: {waktu_tunggu} menit')
             except Exception as e:
-                print(f"Terjadi kesalahan: {e}")
-        else:
-            print("Pilihan tidak valid. Silakan coba lagi.")
+                messagebox.showerror("Kesalahan", f"Terjadi kesalahan: {e}")
+
+    def lihat_data(self):
+        klinik = simpledialog.askstring("Masukkan Nama Poli", "Masukkan nama poli:")
+        nomor_antrean = simpledialog.askinteger("Masukkan Nomor Antrean", "Masukkan nomor antrean:")
+
+        if klinik and nomor_antrean:
+            try:
+                pasien_data = self.server.lihat_data_pasien(klinik, nomor_antrean)
+                messagebox.showinfo("Data Pasien", f'Data Pasien:\n{pasien_data}')
+            except Exception as e:
+                messagebox.showerror("Kesalahan", f"Terjadi kesalahan: {e}")
 
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    app = AntreanMedisGUI(root)
+    root.mainloop()
